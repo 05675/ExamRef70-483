6dbfb811daffb26a41e268ef6a28afa5a637af68
ta kuzuo <j.cole.10@hotmail.co.jp>
2020-09-10 23:37:45 +0900

    First Commit

---
+++ LISTING 1-3 ParallelFor in use/LISTING 1-3 ParallelFor in use/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_3_ParallelFor_in_use
+{
+    class Program
+    {
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        //1番目のパラメータは配列で提供される。
+        //2番目のパラメータはリスト内の各項目で実行されるアクションを提供します。
+        //タスクは開始された順番では完了しないことに注意してください。
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 50).ToArray();
+
+            Parallel.For(0, items.Length, i =>
+            {
+                WorkOnItem(items[i]);
+            });
+
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LISTING 1-4 Managing a parallel For loop/LISTING 1-4 Managing a parallel For loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_4_Managing_a_parallel_For_loop
+{
+    class Program
+    {
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 50).ToArray();
+
+            //ループの実行ステータス
+            ParallelLoopResult result = Parallel.For(0, items.Count(), (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 20)
+                    loopState.Break();
+
+                WorkOnItem(items[i]);
+            });
+
+            Console.WriteLine("Completed: " + result.IsCompleted);      //ループの完了報告
+            Console.WriteLine("Items: " + result.LowestBreakIteration); //どこでブレークされたか判断
+
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+        }
+    }
+}
